ae8a928417fedab6ca6d7a8958256ce7895b82b4
solarmagic <solmagix@gmail.com>
2020-07-16 16:56:40 +0900

    perf: :wheelchair: score 조금더 사용하기 쉽게 변경

--- src/utility/score.ts
+++ src/utility/score.ts
@@ -1,18 +1,25 @@
 import { poseSimilarity } from 'posenet-similarity';
 
-function extractPose(view) {
-    if (!view.hasOwnProperty('poses')) return null;
-    if (!view.poses.hasOwnProperty('poses')) return null;
+export function extractPose(view) {
+    if (!view || !view.poses || !view.poses.poses) return null;
+    // if (!view.poses.hasOwnProperty('poses')) return null;
+    if (view.poses.poses.length > 1) {
+        console.log("multi person detected");
+        return null;
+    }
     return view.poses.poses[0];
 }
 
+// views: Screen.views (view[])
+// return: -1: 현재 모델이 제대로 측정 안됨, 0~1 값 점수 
 export async function scorePoseSimilarity(views) {
-    if (views.length !== 2) return 0;
+    if (views.length !== 2) return -1;
     const modelPose= extractPose(views[0]);
     const userPose = extractPose(views[1]);
-    if (modelPose == null || userPose == null) return 0;
-    
-    const similarityScore = poseSimilarity(modelPose, userPose, { strategy: 'cosineSimilarity' });
-    console.log(similarityScore);
-    return similarityScore;
+    if (modelPose == null) return -1;
+    if (userPose == null) return 0;
+
+    return poseSimilarity(modelPose, modelPose, {strategy: "weightedDistance"}); // 0일수록 비슷 아마 1넘어가기 힘들듯?
+    // return poseSimilarity(modelPose, userPose, {strategy: "cosineDistance"}); // 0일수록 비슷 0~2사이 값 
+    // return poseSimilarity(modelPose, userPose, {strategy: "cosineSimilarity"});// -1~1 사이 값, -1이면 방향 완전 반대, 1이면 완전 똑같음
 }
