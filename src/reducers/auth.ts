a0566e60080c7cc498b9401cc17bd6370abaa49c
김준원 <junis3@snu.ac.kr>
2020-07-30 15:01:57 +0900

    refactor: :building_construction: Redux 구조 얹음

---
+++ src/reducers/auth.ts
@@ -0,0 +1,17 @@
+import * as types from '../actions/ActionTypes';
+
+const initialState = {
+    token: null,
+};
+
+export default function counter(state = initialState, action) {
+    switch (action.type) {
+        case types.AUTH:
+            return {
+                ...state,
+                token: action.token,
+            };
+        default:
+            return state;
+    }
+}
