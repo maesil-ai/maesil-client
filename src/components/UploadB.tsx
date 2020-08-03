8c7b0cff5374d20273b6a67d656f4a63af0a6e5a
김준원 <junis3@snu.ac.kr>
2020-08-03 12:40:13 +0900

    feat: :sparkles: 영상이 업로드될 때 포즈 정보와 같이 올라간다!

--- src/components/UploadB.tsx
+++ src/components/UploadB.tsx
@@ -2,27 +2,40 @@ import { postExercise } from '../utility/api';
 import { validateVideoLength } from '../utility/validation';
 
 import React from 'react';
+import { fps, extractPoseFromVideo } from '../utility/processVideo';
 
 interface UploadBProps {
-    video : Blob,
+    video : File,
 };
 
 export function UploadB({ video } : UploadBProps) {
-  let [title, setTitle] = React.useState("아 배고프다.");
+  let [title, setTitle] = React.useState("[진짜] 포즈와 함께 업로드되고 있는 영상!");
   let [description, setDescription] = React.useState("머 그렇게 만들어진 영상입니다 긴 말 안하겠습니다 이 영상은 개 쩌는 영상입니다 운동효과 완전 개굿입니다");
-  let [message, setMessage] = React.useState("");
+  let [message, setMessage] = React.useState("영상 처리 중...");
+  let [poses, setPoses] = React.useState([]);
   let videoRef = React.useRef(null);
 
   let videoUrl = URL.createObjectURL(video);
 
+  React.useEffect(() => {
+    extractPoseFromVideo(videoUrl).then((poses) => {
+      setMessage("");
+      setPoses(poses);
+      console.log(JSON.stringify(poses));
+    }).catch((error) => {
+      setMessage("영상 처리 실패...");
+      console.log(error);
+    });
+  }, []);
+
   const upload = async () => {
-    const defaultUrl = 'https://maesil-storage.s3.ap-northeast-2.amazonaws.com/images/boyunImage.jpg';
-    const defaultThumbnail = await fetch(defaultUrl, {mode: "no-cors"}).then(r => r.blob());
-    setMessage('업로드 중...');
+    const defaultThumbnailUrl = 'https://maesil-storage.s3.ap-northeast-2.amazonaws.com/images/boyunImage.jpg';
+    const defaultThumbnail = await fetch(defaultThumbnailUrl, {mode: "no-cors"}).then(r => r.blob());
+    setMessage('올리는 중...');
 
     if (!await validateVideoLength(videoRef.current)) {
-      setMessage('비디오는 1초 이상 15초 이하의 길이로 올려');
-      return ;
+      setMessage('영상의 길이는 1초에서 15초 사이여야 합니다.');
+      return;
     }
 
     const success = await postExercise({
@@ -34,7 +47,10 @@ export function UploadB({ video } : UploadBProps) {
       reward: 103,
       tag_id: 2,
       level: 4,
-      skeleton: '',
+      skeleton: JSON.stringify({
+        fps: fps,
+        poses: poses,
+      }),
     });
 
     if (success) {
@@ -56,8 +72,7 @@ export function UploadB({ video } : UploadBProps) {
             설명  
             <input className='inputDescription' value={description} onChange={(e) => { setDescription(e.target.value); }}/>
           </div>
-          <button onClick={upload}> 올리기! </button>
-          <div> { message } </div>
+          { message ? message : <button onClick={upload}> 올리기! </button> }
         </div>
       </div>
   )
