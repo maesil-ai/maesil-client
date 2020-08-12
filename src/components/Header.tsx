fa20c97d1cdb10d003307366f2182119fae01fa7
solarmagic <solarmagic@hanyang.ac.kr>
2020-08-12 15:16:46 +0900

    ci: :white_check_mark: 배포 잘되는지 테스트

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,51 +2,54 @@ import React, { Dispatch } from 'react';
 import { Link } from 'react-router-dom';
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import LoginButton from './LoginButton';
-import {  useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootReducerState } from 'reducers';
 
 function Header() {
-  let userInfo = useSelector((state : RootReducerState) => state.user.userInfo );
+  let userInfo = useSelector((state: RootReducerState) => state.user.userInfo);
 
-  const dropdownMenu = React.useMemo(() => (
-    <li className="dropdown right">
-      <div style={{ padding: '28.5px' }}>
-        <PermIdentityIcon fontSize="large" />
-      </div>
-      <div className="dropdown-content">
-        {userInfo ? (
-          <div className="text"> {userInfo.nickname}님 안녕하세요! </div>
-        ) : (
-          <LoginButton />
-        )}
-        {userInfo && (
-          <div>
-            <Link to="/mypage"> 마이페이지 </Link>
-          </div>
-        )}
-        {userInfo && (
-          <div>
-            <Link to="/upload"> 운동 업로드 </Link>
-          </div>
-        )}
-        {userInfo && (
-          <div>
-            <Link to="/logout"> 로그아웃 </Link>
-          </div>
-        )}
-      </div>
-    </li>
-  ), [userInfo]);
+  const dropdownMenu = React.useMemo(
+    () => (
+      <li className="dropdown right">
+        <div style={{ padding: '28.5px' }}>
+          <PermIdentityIcon fontSize="large" />
+        </div>
+        <div className="dropdown-content">
+          {userInfo ? (
+            <div className="text"> {userInfo.nickname}님 안녕하세요! </div>
+          ) : (
+            <LoginButton />
+          )}
+          {userInfo && (
+            <div>
+              <Link to="/mypage"> 마이페이지 </Link>
+            </div>
+          )}
+          {userInfo && (
+            <div>
+              <Link to="/upload"> 운동 업로드 </Link>
+            </div>
+          )}
+          {userInfo && (
+            <div>
+              <Link to="/logout"> 로그아웃 </Link>
+            </div>
+          )}
+        </div>
+      </li>
+    ),
+    [userInfo]
+  );
 
   return (
     <header>
       <ul>
         <li className="left">
           <Link to="/" style={{ padding: '32px' }}>
-            매실
+            새실
           </Link>
         </li>
-        { dropdownMenu }
+        {dropdownMenu}
       </ul>
     </header>
   );
