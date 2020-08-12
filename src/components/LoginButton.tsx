cb5af3ddfb023cd61f4dc8c520c94d4c0f4a2d38
solarmagic <solarmagic@hanyang.ac.kr>
2020-08-12 13:05:27 +0900

    feat:

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,23 +1,21 @@
 import KakaoLogin from 'react-kakao-login';
-// import { kakaoJsKey } from 'utility/secret';
 import React from 'react';
 import { login, getUserInfo, setAccessToken } from 'utility/api';
 import { Redirect } from 'react-router-dom';
 import { userInfoHasMetadata } from 'utility/types';
 import { useSelector, useStore } from 'react-redux';
 import { RootReducerState } from 'reducers';
-
+import * as dotenv from 'dotenv';
 interface LoginButtonProps {}
 
 const LoginButton = ({}: LoginButtonProps) => {
   let [status, setStatus] = React.useState(0);
   const store = useStore();
-
   if (status == 2) return <Redirect to="/signup" />;
-
+  dotenv.config();
   return (
     <KakaoLogin
-      jsKey={process.env.KAKAO_JS_KEY}
+      jsKey={process.env.REACT_APP_KAKAO_JS_KEY}
       onSuccess={async (response) => {
         setStatus(1);
         if (
