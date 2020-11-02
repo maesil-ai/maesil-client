74a90599ff8fe7b6803ce012e866075d25f17fbf
김준원 <junis3@snu.ac.kr>
2020-11-02 12:58:00 +0900

    feat: :sparkles: '스튜디오'로 가는 통로 지워진거 다시 살림

--- src/components/UserIntroduce.tsx
+++ src/components/UserIntroduce.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import usePromise from 'utility/usePromise';
 import { getSubscribed, toggleSubscribe } from 'utility/api';
+import store from 'store';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootReducerState } from 'reducers';
 
 interface UserIntroduceProps {
     name: string,
@@ -10,6 +14,7 @@ interface UserIntroduceProps {
 export function UserIntroduce({ name, id } : UserIntroduceProps) {
     let [loading, initialSubscribed] = usePromise(() => getSubscribed(id));
     let [subscribed, setSubscribed] = React.useState<boolean>();
+    let myName = useSelector((state: RootReducerState) => state.user.userInfo ? state.user.userInfo.nickname : null );
 
     React.useEffect(() => {
         setSubscribed(initialSubscribed);
@@ -26,7 +31,18 @@ export function UserIntroduce({ name, id } : UserIntroduceProps) {
             <div className='backgroundImage'/>
             <div className='information'>
                 <div className='userName'> {name} </div>
-                <button onClick={onSubscribe}>{ subscribed ? "구독중" : "구독" }</button>
+                { name != myName && (
+                    <button onClick={onSubscribe} className={subscribed ? 'inactive' : 'active'}>
+                        { subscribed ? "구독중" : "구독" }
+                    </button>
+                )}  
+                { name == myName && (
+                    <Link to='/studio'> 
+                        <button className='active'> 
+                            운동 올리기 
+                        </button>
+                    </Link>
+                )}
             </div>
         </div>
     );
