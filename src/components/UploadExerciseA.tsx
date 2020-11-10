3d2f2d50598cb8811883fcf07ddedfa496dc1d2e
김준원 <junis3@snu.ac.kr>
2020-11-10 17:10:08 +0900

    feat: :art: 업로드 그림넣음~

--- src/components/UploadExerciseA.tsx
+++ src/components/UploadExerciseA.tsx
@@ -16,16 +16,30 @@ export function UploadExerciseA({ onFinish }: UploadExerciseAProps) {
 
   return (
     <div {...getRootProps({ className: 'zone' })}>
-      <div style={{paddingBottom: '200px'}} />
+      <div style={{paddingBottom: '110px'}} />
       <input {...getInputProps()} />
       {isDragActive ? (
-        <p>여기로 파일을 드래그해 주세요.</p>
+        <p>
+          <div>
+            <img src='https://maesil-storage.s3.ap-northeast-2.amazonaws.com/upload-file.png' height={150} />
+          </div>
+          <div>
+            <br/>
+            여기로 파일을 드래그해 주세요.
+          </div>
+        </p>
       ) : (
         <p>
-          파일을 여기로 드래그하거나, 상자를 눌러서 운동 영상을 올려 주세요.
+          <div>
+            <img src='https://maesil-storage.s3.ap-northeast-2.amazonaws.com/upload-file.png' height={150} />
+          </div>
+          <div>
+          <br/>
+            파일을 여기로 드래그하거나, 상자를 눌러서 운동 영상을 올려 주세요.
+          </div>
         </p>
       )}
-      <div style={{paddingBottom: '200px'}} />
+      <div style={{paddingBottom: '110px'}} />
     </div>
   );
 }
